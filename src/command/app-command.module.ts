f7141d7171a62f4202ab70cf65bf462d85a9c048
thanhlucvip2 <doanthanhluc91bvh@gmail.com>
2024-11-29 13:37:44 +0700

    setup command

---
+++ src/command/app-command.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CommandModule } from 'nestjs-command';
+
+import { CryptoKeyCommand } from './crypto-key.command';
+
+@Module({
+  imports: [CommandModule],
+  providers: [CryptoKeyCommand],
+})
+export class AppCommandModule {}
--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,3 +1,6 @@
-import "dotenv/config"
+import 'dotenv/config';
 
 export const PORT = process.env.PORT || 5000;
+export const CIPHER_KEY = process.env.CIPHER_KEY;
+export const CIPHER_IV = process.env.CIPHER_IV;
+export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
